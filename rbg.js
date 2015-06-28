8cbd99fdbae136035b52c503a6545cc3df5086cd
Reuben <reuben.telushkin@gmail.com>
2015-06-28 17:02:53 -0400

    added fade effect via new file rbg.js

---
+++ rbg.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('div').fadeOut(1);
+  // console.log("fade out works");
+  $('div').fadeIn(1000);
+  // console.log("fade in works");
+  $(document).getElementsByTagName('box')
+  
+
+});
